1bfccb866638b2964e0a5cffb42838ae00793e2e
Vadim Rudakov <vadim@rudakov.pw>
2018-02-13 23:07:00 +0300

    1.1. Создание Hello World[#141]

---
+++ src/ru/job4j/Calculate.java
@@ -0,0 +1,18 @@
+package ru.job4j;
+
+/**
+ * Calculate.
+ *
+ * @author Vadim Rudakov (vadim@rudakov.pw)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Calculate {
+    /**
+     * Main.
+     * @param args - args.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
