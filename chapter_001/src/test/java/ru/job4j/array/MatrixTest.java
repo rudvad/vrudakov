db19e477d290893c41bce80ad31c0a734ba4be71
Vadim Rudakov <vadim@rudakov.pw>
2018-03-06 01:58:27 +0300

    5.4. Двухмерный массив. Таблица умножения. [#33491]

--- chapter_001/src/test/java/ru/job4j/array/MatrixTest.java
+++ chapter_001/src/test/java/ru/job4j/array/MatrixTest.java
@@ -10,10 +10,11 @@ public class MatrixTest {
 	public void whenSizeIsThree() {
 		Matrix matrix = new Matrix();
         int[][] result = matrix.multiple(3);
-        int[][] expected = new int[][] {{0, 1, 2, 3},
-										{1, 1, 2, 3},
-										{2, 2, 4, 6}, 
-										{3, 3, 6, 9}};
+        int[][] expected = new int[][] {
+		{0, 1, 2, 3},
+		{1, 1, 2, 3},
+		{2, 2, 4, 6}, 
+		{3, 3, 6, 9}};
         assertThat(result, is(expected));
 		}
 		
@@ -21,12 +22,13 @@ public class MatrixTest {
 	public void whenSizeIsFive() {
 		Matrix matrix = new Matrix();
         int[][] result = matrix.multiple(5);
-        int[][] expected = new int[][] {{0, 1, 2, 3, 4, 5}, 
-										{1, 1, 2, 3, 4, 5}, 
-										{2, 2, 4, 6, 8, 10}, 
-										{3, 3, 6, 9, 12, 15}, 
-										{4, 4, 8, 12, 16, 20}, 
-										{5, 5, 10, 15, 20, 25}};
+        int[][] expected = new int[][] {
+		{0, 1, 2, 3, 4, 5}, 
+		{1, 1, 2, 3, 4, 5}, 
+		{2, 2, 4, 6, 8, 10}, 
+		{3, 3, 6, 9, 12, 15}, 
+		{4, 4, 8, 12, 16, 20}, 
+		{5, 5, 10, 15, 20, 25}};
         assertThat(result, is(expected));
 		}
 }
